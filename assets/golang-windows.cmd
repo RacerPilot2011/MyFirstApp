c3c68bf5218594d90bc19785b05bc861568207ff
RacerPilot2011 <101601283+RacerPilot2011@users.noreply.github.com>
2023-01-10 17:01:12 -0600

    Accually made it install. Can't test to be sure.

--- assets/golang-windows.cmd
+++ assets/golang-windows.cmd
@@ -1,1 +1,16 @@
-@ECHO OFF
+echo on
+echo "Installing golang..."
+echo off
+try
+{
+    ttsBegin;
+    wget https://go.dev/dl/go1.19.5.windows-amd64.msi;
+    ttscommit;
+}
+catch
+{
+    echo on
+    echo "You do not have wget installed.Install and start the program again. Redirecting you to the downloads page..."
+    echo off
+    START https://eternallybored.org/misc/wget/
+}
